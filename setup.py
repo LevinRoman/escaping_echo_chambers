24b30db1a8fe28cdc5c1f606f54acfcba2a40a21
LevinRoman <romalevi@yandex.ru>
2020-10-01 16:55:46 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='the diversity of content they are exposed to by recommender systems and online services. The tool allows to increase this diversity to escape echo chambers and filter bubbles.',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
